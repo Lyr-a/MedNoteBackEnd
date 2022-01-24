830e52c54acade7c8326b245599fe77e9e749f73
Estefanosb <87233013+Estefanosb@users.noreply.github.com>
2022-01-24 12:43:04 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 
 //conectar ao bd
-$con = pg_connect(getenv("DATABASE_URL"));
+$con = pg_connect("postgres://rfvpbzdy:Viurc8sZ2VdqcnPCsFCOd9j9a-qFAOMG@chunee.db.elephantsql.com/rfvpbzdy");
 
 //array para resposta json
 $response = array();
