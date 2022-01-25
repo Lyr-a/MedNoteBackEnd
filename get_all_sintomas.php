41ce529c317596adcf2dfacdcedb17be0fe6a0ba
Estefanosb <87233013+Estefanosb@users.noreply.github.com>
2022-01-25 16:58:51 -0300

    Update get_all_sintomas.php

--- get_all_sintomas.php
+++ get_all_sintomas.php
@@ -7,41 +7,68 @@ $response = array();
  
 
 $con = pg_connect("postgres://rfvpbzdy:Viurc8sZ2VdqcnPCsFCOd9j9a-qFAOMG@chunee.db.elephantsql.com/rfvpbzdy");
- 
+
+
+$CPF = NULL;
+$SENHA = NULL;
+
+$isAuth = false;
+
+// Método para mod_php (Apache)
+if(isset( $_SERVER['PHP_AUTH_USER'])) {
+    $CPF = $_SERVER['PHP_AUTH_USER'];
+    $SENHA = $_SERVER['PHP_AUTH_PW'];
+} // Método para demais servers
+elseif(isset( $_SERVER['HTTP_AUTHORIZATION'])) {
+    if(preg_match( '/^basic/i', $_SERVER['HTTP_AUTHORIZATION']))
+		list($CPF, $SENHA) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+}
+
+// Se a autenticação não foi enviada
+if(!is_null($CPF)){
+    $query = pg_query($con, "SELECT SENHA FROM Pessoa WHERE CPF='$CPF'");
+
+	if(pg_num_rows($query) > 0){
+		$row = pg_fetch_array($query);
+		if($SENHA == $row['SENHA']){
+			$isAuth = true;
+		}
+	}
+}
+if($isAuth) {
 // Realiza uma consulta ao BD e obtem todos os produtos.
-$result = pg_query($con, "SELECT *FROM Sintoma");
+	$result = pg_query($con, "SELECT * FROM Sintoma WHERE CPF='$CPF'");
  
 
-if (pg_num_rows($result) > 0) {
-   
-    $response["Sintoma"] = array();
- 
-    while ($row = pg_fetch_array($result)) {
-     
-        $sintomas = array();
-        $sintomas["ID_SINTOMA"] = $row["ID_SINTOMA"];
-        $sintomas["SINTOMA_TITLE"] = $row["SINTOMA_TITLE"];
- 
-        // Adiciona o produto no array de produtos.
-        array_push($response["Sintoma"], $sintomas);
-    }
-   
-    $response["success"] = 1;
-	
-	pg_close($con);
- 
-    // Converte a resposta para o formato JSON.
-    echo json_encode($response);
-	
-} else {
-    
-    $response["success"] = 0;
-    $response["message"] = "Nao ha sintomas";
-	
-	// Fecha a conexao com o BD
-	pg_close($con);
- 
-    // Converte a resposta para o formato JSON.
-    echo json_encode($response);
+	if (pg_num_rows($result) > 0) {
+	   
+		$response["Sintoma"] = array();
+	 
+		while ($row = pg_fetch_array($result)) {
+		 
+			$sintomas = array();
+			$sintomas["ID_SINTOMA"] = $row["ID_SINTOMA"];
+			$sintomas["SINTOMA_TITLE"] = $row["SINTOMA_TITLE"];
+	 
+			// Adiciona o produto no array de produtos.
+			array_push($response["Sintoma"], $sintomas);
+		}
+	   
+		$response["success"] = 1;
+		
+
+		
+	} else {
+		
+		$response["success"] = 0;
+		$response["message"] = "Nao ha sintomas";
+		
+		// Fecha a conexao com o BD
+		
+	}
 }
+pg_close($con);
+	 
+// Converte a resposta para o formato JSON.
+echo json_encode($response);
 ?>
