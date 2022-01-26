43b29393f44667276aec55b324d2becd45931ac9
Estefanosb <87233013+Estefanosb@users.noreply.github.com>
2022-01-26 02:14:32 -0300

    Update create_tratamento.php

--- create_tratamento.php
+++ create_tratamento.php
@@ -5,12 +5,13 @@
 $response = array();
 
 //conferir os campos requisitados
-if (isset($_POST['TRATAMENTO_TITLE']) && isset($_POST['TRATAMENTO_DESC'])  && isset($_POST['TRATAMENTO_DATA']) && isset($_FILES['TRATAMENTO_HORA'])){
+if (isset($_POST['tratamento_title']) && isset($_POST['tratamento_desc'])  && isset($_POST['tratamento_data']) && isset($_FILES['tratamento_hora'])){
 
    
-	$TRATAMENTO_TITLE = $_POST['TRATAMENTO_TITLE'];
-	$TRATAMENTO_DESC = $_POST['TRATAMENTO_DESC'];
-	$TRATAMENTO_DATA = $_POST['TRATAMENTO_DATA'];
+	$TRATAMENTO_TITLE = $_POST['tratamento_title'];
+	$TRATAMENTO_DESC = $_POST['tratamento_desc'];
+	$TRATAMENTO_DATA = $_POST['tratamento_data'];
+	$TRATAMENTO_HORA = $_POST['tratamento_hora'];
 	
 
 
@@ -22,7 +23,7 @@ if (isset($_POST['TRATAMENTO_TITLE']) && isset($_POST['TRATAMENTO_DESC'])  && is
 //conectar ao banco de dados
 	$con = pg_connect("postgres://rfvpbzdy:Viurc8sZ2VdqcnPCsFCOd9j9a-qFAOMG@chunee.db.elephantsql.com/rfvpbzdy");
 	
-	$result = pg_query($con, "INSERT INTO Tratamento(TRATAMENTO_TITLE, TRATAMENTO_DESC, TRATAMENTO_DATA)
+	$result = pg_query($con, "INSERT INTO Tratamento(tratamento_title, tratamento_desc, tratamento_data, tratamento_hora )
 	VALUES('$TRATAMENTO_TITLE', '$TRATAMENTO_DESC','$TRATAMENTO_DATA')");
 	
 	if ($result){
