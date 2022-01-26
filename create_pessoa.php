04bb3dbafe5fabd760e435259624470b65a5fec2
Estefanosb <87233013+Estefanosb@users.noreply.github.com>
2022-01-26 01:37:17 -0300

    Update create_pessoa.php

--- create_pessoa.php
+++ create_pessoa.php
@@ -27,8 +27,8 @@ if (isset($_POST['cpf']) && isset($_POST['senha']) && isset($_POST['nome']) && i
 	}
 	
 	else{	
-		$result = pg_query($con, "INSERT INTO Pessoa(cpf, senha, nome, genero, tipo_sang, num_emer)
-		VALUES('$CPF', '$SENHA','$NOME','$GENERO', '$TIPO_SANG','$NUM_EMER')");
+		$result = pg_query($con, "INSERT INTO Pessoa(cpf, senha, nome, genero, tipo_sang, data_nasc, num_emer)
+		VALUES('$CPF', '$SENHA','$NOME','$GENERO', '$TIPO_SANG', $DATA_NASC, '$NUM_EMER')");
 	
 		if ($result){
 			$response["success"] = 1;
