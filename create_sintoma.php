b80a06fa9a31eabe811dcf170062eac119755ad5
Estefanosb <87233013+Estefanosb@users.noreply.github.com>
2022-01-27 10:33:25 -0300

    Update create_sintoma.php

--- create_sintoma.php
+++ create_sintoma.php
@@ -5,10 +5,10 @@
 $response = array();
 
 //conferir os campos requisitados
-if (isset($_POST['sintoma_title']) && isset($_POST['sintoma_desc']) && isset($_POST['sintoma_data'])&& isset($_POST['sintoma_hora'])&& isset($_FILES['sintoma_photo'])){
+if (isset($_POST['sintoma_title']) && (isset($_POST['sintoma_title']) && isset($_POST['sintoma_desc']) && isset($_POST['sintoma_data']) && isset($_POST['sintoma_hora'])){
 
 	
-	
+	$CPF = trim($_POST['sintoma_title']);
 	$SINTOMA_TITLE = $_POST['sintoma_title'];
 	$SINTOMA_DESC = $_POST['sintoma_desc'];
 	$SINTOMA_DATA = $_POST['sintoma_data'];
@@ -20,24 +20,19 @@ if (isset($_POST['sintoma_title']) && isset($_POST['sintoma_desc']) && isset($_P
 	//conectar ao banco de dados
 	$con = pg_connect("postgres://rfvpbzdy:Viurc8sZ2VdqcnPCsFCOd9j9a-qFAOMG@chunee.db.elephantsql.com/rfvpbzdy");
 	
-	$result = pg_query($con, "INSERT INTO Sintoma(sintoma_title, sintoma_desc, sintoma_data, sintoma_hora, sintoma_photo)
-	VALUES('$SINTOMA_TITLE', '$SINTOMA_DESC','$SINTOMA_DATA','$SINTOMA_HORA','$SINTOMA_PHOTO')");
+	$result = pg_query($con, "INSERT INTO Sintoma(cpf, sintoma_title, sintoma_desc, sintoma_data, sintoma_hora, sintoma_photo)
+	VALUES('$CPF', '$SINTOMA_TITLE', '$SINTOMA_DESC','$SINTOMA_DATA','$SINTOMA_HORA')");
 	
 	if ($result){
 	$response["success"] = 1;
 	$response["message"] = "Sintoma criado com sucesso";
 	
-	pg_close($con);
-	
-	echo json_encode($response); }
-	
+	}
 	else{
 	$response["success"] = 0;
 	$response["message"] = "Erro ao criar sintoma no bd";
 	
-	pg_close($con);
-	
-	echo json_encode($response);
+
 	
 	}
 }
@@ -45,9 +40,11 @@ else {
 	$response["success"] = 0;
 	$response["message"] = "Campo requerido nao preenchido";
 	
-	echo json_encode($response);
+
 	
 }
+pg_close($con);
+echo json_encode($response);
 ?>
 	
 	
