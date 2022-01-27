f31a59bfd9a8db2865dfbedf2047a9f40170addf
Estefanosb <87233013+Estefanosb@users.noreply.github.com>
2022-01-27 10:47:49 -0300

    Update create_tratamento.php

--- create_tratamento.php
+++ create_tratamento.php
@@ -1,13 +1,14 @@
 <?php 
 
-//tratamento
+// criar tratamento
 
 $response = array();
 
 //conferir os campos requisitados
-if (isset($_POST['tratamento_title']) && isset($_POST['tratamento_desc'])  && isset($_POST['tratamento_data']) && isset($_FILES['tratamento_hora'])){
+if (isset($_POST['cpf']) && isset($_POST['tratamento_title']) && isset($_POST['tratamento_desc'])  && isset($_POST['tratamento_data']) && isset($_FILES['tratamento_hora'])){
 
-   
+	
+   	$CPF = trim($_POST['cpf']);
 	$TRATAMENTO_TITLE = $_POST['tratamento_title'];
 	$TRATAMENTO_DESC = $_POST['tratamento_desc'];
 	$TRATAMENTO_DATA = $_POST['tratamento_data'];
@@ -23,24 +24,20 @@ if (isset($_POST['tratamento_title']) && isset($_POST['tratamento_desc'])  && is
 //conectar ao banco de dados
 	$con = pg_connect("postgres://rfvpbzdy:Viurc8sZ2VdqcnPCsFCOd9j9a-qFAOMG@chunee.db.elephantsql.com/rfvpbzdy");
 	
-	$result = pg_query($con, "INSERT INTO Tratamento(tratamento_title, tratamento_desc, tratamento_data, tratamento_hora )
-	VALUES('$TRATAMENTO_TITLE', '$TRATAMENTO_DESC','$TRATAMENTO_DATA')");
+	$result = pg_query($con, "INSERT INTO Tratamento(cpf, tratamento_title, tratamento_desc, tratamento_data, tratamento_hora )
+	VALUES('$CPF', '$TRATAMENTO_TITLE', '$TRATAMENTO_DESC','$TRATAMENTO_DATA', '$TRATAMENTO_HORA')");
 	
 	if ($result){
 	$response["success"] = 1;
 	$response["message"] = "tratamento criado com sucesso";
 	
-	pg_close($con);
-	
-	echo json_encode($response); }
+	}
 	
 	else{
 	$response["success"] = 0;
 	$response["message"] = "Erro ao criar tratamento no bd";
 	
-	pg_close($con);
-	
-	echo json_encode($response);
+
 	
 	}
 }
@@ -48,7 +45,10 @@ else {
 	$response["success"] = 0;
 	$response["message"] = "Campo requerido nao preenchido";
 	
-	echo json_encode($response);
+	
 	
 }
+pg_close($con);
+	
+echo json_encode($response);
 ?>
