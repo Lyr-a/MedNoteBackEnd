535602d3224f26b04c24d567fb61e4593985dca8
Estefanosb <87233013+Estefanosb@users.noreply.github.com>
2022-01-27 11:46:21 -0300

    Update create_tfoto.php

--- create_tfoto.php
+++ create_tfoto.php
@@ -5,11 +5,11 @@
 $response = array();
 
 //conferir os campos requisitados
-if (isset($_POST['FOTO'])){
+if (isset($_POST['foto'])){
 
    
-	$imageFileType = strtolower(pathinfo(basename($_FILES["FOTO"]["name"]), PATHINFO_EXTENSION));
-	$image_base64 = base64_encode(file_get_contents($_FILES['FOTO']['tmp_name']));
+	$imageFileType = strtolower(pathinfo(basename($_FILES["foto"]["name"]), PATHINFO_EXTENSION));
+	$image_base64 = base64_encode(file_get_contents($_FILES['foto']['tmp_name']));
 	$FOTO = 'data:image/'.$imageFileType';base64,'.$image_base64;
 
 
@@ -17,23 +17,17 @@ if (isset($_POST['FOTO'])){
 //conectar ao banco de dados
 	$con = pg_connect("postgres://rfvpbzdy:Viurc8sZ2VdqcnPCsFCOd9j9a-qFAOMG@chunee.db.elephantsql.com/rfvpbzdy");
 	
-	$result = pg_query($con, "INSERT INTO TFotos(FOTO) VALUES('$FOTO')");
+	$result = pg_query($con, "INSERT INTO TFotos(foto) VALUES('$FOTO')");
 	
 	if ($result){
 	$response["success"] = 1;
 	$response["message"] = "foto criada com sucesso";
 	
-	pg_close($con);
-	
-	echo json_encode($response); }
 	
 	else{
 	$response["success"] = 0;
 	$response["message"] = "Erro ao criar foto no bd";
-	
-	pg_close($con);
-	
-	echo json_encode($response);
+
 	
 	}
 }
@@ -41,7 +35,10 @@ else {
 	$response["success"] = 0;
 	$response["message"] = "Campo requerido nao preenchido";
 	
-	echo json_encode($response);
+
 	
 }
+pg_close($con);
+	
+echo json_encode($response); }
 ?>
