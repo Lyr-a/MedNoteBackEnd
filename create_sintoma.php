361a755038c264b728517c537f75d087cbb41328
L\y/R <88154692+Lyr-a@users.noreply.github.com>
2022-01-28 11:03:26 -0300

    Update create_sintoma.php

--- create_sintoma.php
+++ create_sintoma.php
@@ -8,7 +8,7 @@ $response = array();
 	$con = pg_connect("postgres://rfvpbzdy:Viurc8sZ2VdqcnPCsFCOd9j9a-qFAOMG@chunee.db.elephantsql.com/rfvpbzdy");	
 
 //conferir os campos requisitados
-if (isset($_POST['cpf']) && isset($_POST['sintoma_title']) && isset($_POST['sintoma_desc']) && isset($_POST['sintoma_data']) && isset($_POST['sintoma_hora']) && isset($_FILES['sintoma_photo'])){
+if (isset($_POST['cpf']) && isset($_POST['sintoma_title']) && isset($_POST['sintoma_desc']) && isset($_POST['sintoma_data']) && isset($_POST['sintoma_hora'])){
 
 	
 	$CPF = trim($_POST['cpf']);
@@ -17,11 +17,11 @@ if (isset($_POST['cpf']) && isset($_POST['sintoma_title']) && isset($_POST['sint
 	$SINTOMA_DATA = $_POST['sintoma_data'];
 	$SINTOMA_HORA = $_POST['sintoma_hora'];
 	
-	
+	/*
 	$imageFileType = strtolower(pathinfo(basename($_FILES["sintoma_photo"]['name']), PATHINFO_EXTENSION));
 	$image_base64 = base64_encode(file_get_contents($_FILES['sintoma_photo']['tmp_name']));
 	$SINTOMA_PHOTO = 'data:image/'.$imageFileType';base64,'.$image_base64;
-
+*/
 	
 	$result = pg_query($con, "INSERT INTO Sintoma(cpf, sintoma_title, sintoma_desc, sintoma_data, sintoma_hora, sintoma_photo)
 	VALUES('$CPF', '$SINTOMA_TITLE', '$SINTOMA_DESC','$SINTOMA_DATA','$SINTOMA_HORA', '$SINTOMA_PHOTO')");
